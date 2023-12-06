d2ddd466c970483f72519ea77cebec3f4bd6a0b1
bhargav <karandave503@gmail.com>
2023-12-06 19:53:22 +0530

    change color

--- src/Components/ImageLoader.jsx
+++ src/Components/ImageLoader.jsx
@@ -1,6 +1,6 @@
 const ImageLoader = () => {
   return (
-    <div className="flex space-x-2 justify-center items-center dark:invert">
+    <div className="flex space-x-2 justify-center items-center ">
       <span className="sr-only">Loading...</span>
       <div className="h-8 w-8 bg-[#fff] rounded-full animate-bounce [animation-delay:-0.3s]"></div>
       <div className="h-8 w-8 bg-[#fff]  rounded-full animate-bounce [animation-delay:-0.15s]"></div>
